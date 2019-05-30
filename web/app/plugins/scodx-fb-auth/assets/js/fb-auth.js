2d06c317c28fe8b2fd590a8e488ab4aac4c7aa7d
Oscar Sanchez <oscar.exe@gmail.com>
2019-05-29 22:42:02 -0500

    adding fb auth plugin

---
+++ web/app/plugins/scodx-fb-auth/assets/js/fb-auth.js
@@ -0,0 +1,41 @@
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '2218130684950042',
+    cookie     : true,
+    xfbml      : true,
+    version    : 'v3.3'
+  });
+  FB.AppEvents.logPageView();
+};
+
+(function(d, s, id){
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {return;}
+  js = d.createElement(s); js.id = id;
+  js.src = "https://connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+function checkLoginState() {
+  FB.getLoginStatus(function(response) {
+    if (response.status === 'connected') {
+      // obtaining fb user name and email to send it to wp
+      FB.api('/me', {fields: 'name,email'}, function(fbData) {
+        var data = {
+          action: 'fb_auth_init',
+          user_name: fbData.name,
+          user_email: fbData.email,
+          user_id: fbData.id,
+        };
+        jQuery.post(fb_auth.ajaxurl, data, function(fbAuthResponse) {
+          if (fbAuthResponse.user_id) {
+            window.location = fbAuthResponse.admin_url;
+          }
+        });
+        return false;
+      });
+
+    }
+  });
+}
+
--- web/app/plugins/scodx-fb-auth/scodx-fb-auth.php
+++ web/app/plugins/scodx-fb-auth/scodx-fb-auth.php
@@ -9,5 +9,135 @@
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  */
 
+define('FB_ADMIN', array(
+  array(
+    'name' => 'Oscar Sanchez',
+    'email' => 'oscar.exe@gmail.com',
+  )
+));
+define('FB_AUTH_TABLE_ADMINS', 'scodx_fb_auth_admins');
 
-// sorry, didn't have time to create this functionality :(
+function scodx_fb_auth_add_fb() {
+  ?>
+  <script>
+  </script>
+
+<!--  <div class="fb-login-button" data-width="" data-size="large" data-button-type="login_with" data-auto-logout-link="false" data-use-continue-as="false"></div>-->
+  <fb:login-button
+    data-size="large"
+    scope="public_profile,email"
+    onlogin="checkLoginState();">
+  </fb:login-button>
+
+  <?php
+}
+
+function scodx_fb_auth_ajax_init() {
+  if ( isset( $_POST['user_email'] ) ) {
+    wp_send_json(scodx_fb_auth_process_auth($_POST));
+  }
+}
+
+function scodx_fb_auth_process_auth($user_data) {
+  $user_email = $user_data['user_email'];
+  $user_name = $user_data['user_name'];
+  $user_fb_id = $user_data['user_id'];
+  $is_fb_admin = !empty(scodx_fb_auth_validate_admin($user_email));
+  $email_exist = email_exists($user_email);
+
+  $create_and_login = function ($email_exist, $role) use ($user_name, $user_email) {
+    if ($email_exist) {
+      // if exist, $email_exist is the user id
+      $user_id = $email_exist;
+    } else {
+      // create user
+      $user_id = wp_create_user(sanitize_user($user_name, true), wp_generate_password(), $user_email );
+    }
+    // process role
+    scodx_fb_auth_process_roles($user_id, $role);
+    // sets auth cookie
+    scodx_fb_auth_login_wp_user($user_id);
+
+    return $user_id;
+  };
+
+  $role = $is_fb_admin ? 'administrator' : 'subscriber';
+  $user_id = $create_and_login($email_exist, $role);
+
+  return [
+    'user_id' => $user_id,
+    'admin_url' => admin_url(),
+  ];
+}
+
+function scodx_fb_auth_process_roles($user_id, $role) {
+  $user = get_userdata( $user_id );
+  $user_roles = $user->roles;
+  if ( !in_array( $role, $user_roles, true ) ) {
+    $user->add_role($role);
+    // If you want to remove all other roles then use this line:
+    // $user->set_role($role);
+    // otherwise with ->add_role will just add the role and keep the previous ones
+  }
+}
+
+function scodx_fb_auth_login_wp_user($user_id) {
+  wp_set_auth_cookie($user_id);
+}
+
+function scodx_fb_auth_validate_admin($email) {
+  global $wpdb;
+  $table_name = $wpdb->prefix . FB_AUTH_TABLE_ADMINS;
+  $query = $wpdb->prepare("SELECT id, name, email from {$table_name} WHERE email = %s", [$email]);
+  return $wpdb->get_row($query);
+}
+
+function scodx_fb_auth_enqueue_script() {
+  wp_enqueue_script( 'scodx-fb-auth-js', plugins_url( 'assets/js/fb-auth.js', __FILE__ ),  array('jquery'));
+  wp_localize_script( 'scodx-fb-auth-js', 'fb_auth', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) );
+}
+
+function scodx_fb_auth_install() {
+  global $wpdb;
+
+  $table_name = $wpdb->prefix . FB_AUTH_TABLE_ADMINS;
+
+  $charset_collate = $wpdb->get_charset_collate();
+
+  $sql = "CREATE TABLE $table_name (
+		id mediumint(9) NOT NULL AUTO_INCREMENT,
+		name tinytext NOT NULL,
+		email text NOT NULL,
+		PRIMARY KEY  (id)
+	) $charset_collate;";
+
+  require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+  dbDelta($sql);
+}
+
+function scodx_fb_auth_install_data() {
+  global $wpdb;
+
+  $table_name = $wpdb->prefix . FB_AUTH_TABLE_ADMINS;
+
+  foreach (FB_ADMIN as $admins) {
+    $wpdb->insert(
+      $table_name,
+      array(
+        'name' => $admins['name'],
+        'email' => $admins['email'],
+      )
+    );
+  }
+}
+
+
+register_activation_hook( __FILE__, 'scodx_fb_auth_install' );
+register_activation_hook( __FILE__, 'scodx_fb_auth_install_data' );
+
+add_action('login_enqueue_scripts', 'scodx_fb_auth_enqueue_script', 1);
+
+add_action('login_form', 'scodx_fb_auth_add_fb');
+
+add_action('wp_ajax_fb_auth_init', 'scodx_fb_auth_ajax_init');
+add_action('wp_ajax_nopriv_fb_auth_init', 'scodx_fb_auth_ajax_init');
